df5fb7beb35967ffa0026c090b781cb4fa702151
sumarokov-vp <sumarokov.vp@gmail.com>
2022-12-12 21:09:49 +0600

    1

--- sl_creatio_connector/creatio.py
+++ sl_creatio_connector/creatio.py
@@ -1,24 +1,13 @@
 import json
 import requests
 from enum import Enum
-from constants import ODATA_version, OrderDirection, FilterOperators
+from sl_creatio_connector.constants import ODATA_version, OrderDirection, FilterOperators
+from typing import List
 
 class Creatio():
 
-    class Filter(object):
-        def __init__(self, field: str|None = None, value:str|None = None, operator: FilterOperators = FilterOperators.and_operator):
-            self.operator = operator,
-            self.data = []
-            if field is not None:
-                self.data.append({'field': field, 'value': value})
-        
-        def add(self, field: str, value:str):
-            self.data.append({'field': field, 'value': value})
-
-
     def __init__(self, creatio_host, login, password, odata_version):
         self.creatio_url = creatio_host
-        self.filter= self.Filter()
         self.odata_version = odata_version
         self.odata_service_link = self.creatio_url + odata_version.value['service_path']
         self.headers = odata_version.value['headers']
@@ -29,25 +18,6 @@ class Creatio():
         else:
             raise Exception(text)
     
-    def generate_filter(self) -> str|None:
-        def id_value(id:str)->str|None:
-            match self.odata_version:
-                case ODATA_version.v3:
-                    return f"guid'{id}'"
-                case ODATA_version.v4 | ODATA_version.v4core:
-                    return id
-                case _:
-                    return None
-        if len(self.filter.data) ==0: return None
-
-        result =f'?$filter=' 
-        for filter in self.filter.data:
-            result += filter['']
-
-        url = self.odata_service_link + f"/ContactCommunicationCollection?$filter=CommunicationType/Id eq guid'{creatio_channel_id}' and Number eq '{number}'"
-        url = self.odata_service_link + f"/ContactCommunication?$filter=CommunicationType/Id eq {creatio_channel_id} and Number eq '{number}'"
-
-
     def forms_auth(self, login, password):
         """ Аутентификация ODATA """
         url = f'{self.creatio_url}/ServiceModel/AuthService.svc/Login'
@@ -64,7 +34,7 @@ class Creatio():
         else:
             return False, response_data['Message']
 
-    def create_object(self, object_name, data):
+    def create_object(self, object_name: str, data: dict)-> dict:
         """ CREATE запрос в Creatio """
         if self.odata_version == ODATA_version.v3:
             url = self.odata_service_link + f"/{object_name}Collection"
@@ -86,174 +56,124 @@ class Creatio():
         return object
 
 
-    def delete_object(self, object_name: str, object_id: str):
+    def delete_object(self, object_name: str, object_id: str) -> requests.Response:
         """ DELETE запрос к Creatio """
-        if self.odata_version == ODATA_version.v3:
-            url = self.odata_service_link + f"/{object_name}Collection(guid'{object_id}')"
-        else:
-            url = self.odata_service_link + f"/{object_name}({object_id})"
+        match self.odata_version:
+            case ODATA_version.v3:
+                url = self.odata_service_link + f"/{object_name}Collection(guid'{object_id}')"
+            case ODATA_version.v4 | ODATA_version.v4core:
+                url = self.odata_service_link + f"/{object_name}({object_id})"
+            case _:
+                raise Exception(f'ODATA version is not supported: {self.odata_version}')
 
         response = requests.delete(
             url=url,
             headers=self.headers,
             cookies= self.cookies,
         )
-        return response.status_code
+        return response
+
+    def get_object_collection(self, object_name:str, parameters: List[str] = []) -> List:
+        _params: str = ''
+        for parameter in parameters:
+            _params += f'?${parameter}'
+
+        match self.odata_version:
+            case ODATA_version.v3:
+                url = self.odata_service_link + f"/{object_name}Collection{_params}"
+                response = requests.get(
+                    url=url,
+                    headers=self.headers,
+                    cookies= self.cookies,
+                )
+                result = json.loads(response.content)['d']['results']
+            case ODATA_version.v4 | ODATA_version.v4core:
+                url = self.odata_service_link + f"/{object_name}{_params}"
+                response = requests.get(
+                    url=url,
+                    headers=self.headers,
+                    cookies= self.cookies,
+                )
+                result = json.loads(response.content)['value']
+            case _:
+                result = [None]
+        return result
+    
+    def get_object_by_id(self, object_name:str, object_id: str):
+        match self.odata_version:
+            case ODATA_version.v3:
+                url = self.odata_service_link + f"/{object_name}Collection(guid'{object_id}')"
+                response = requests.get(
+                    url=url,
+                    headers=self.headers,
+                    cookies= self.cookies,
+                )
+                result = json.loads(response.content)['d']
+            case ODATA_version.v4 | ODATA_version.v4core:
+                url = self.odata_service_link + f"/{object_name}({object_id})"
+                response = requests.get(
+                    url=url,
+                    headers=self.headers,
+                    cookies= self.cookies,
+                )
+                result = json.loads(response.content)
+            case _:
+                result = None
+        return result
+    
+    # Typical implementations
+
+    def get_contact_by_id(self, contact_id: str)->dict|None:
+        """
+            Get contact object (dict type) by Id column
+        """
+        return self.get_object_by_id(
+            object_name= 'Contact',
+            object_id= contact_id
+        )
+    
+    def get_creatio_contact_id(self, creatio_channel_id: str, number: str)->str|None:
+        """
+            Get contactId by phone number or other communication option
+        """
+        parameters = []
+        match self.odata_version:
+            case ODATA_version.v3:
+                parameters = [f"filter=CommunicationType/Id eq guid'{creatio_channel_id}' and Number eq '{number}'"]
+            case ODATA_version.v4 | ODATA_version.v4core:
+                parameters = [f"filter=CommunicationType/Id eq {creatio_channel_id} and Number eq '{number}'"]
+        result= self.get_object_collection(
+            object_name= 'ContactCommunication',
+            parameters= parameters,
+        )[0]        
+        if result is None:
+            return None
+        else:
+            return result['ContactId']
 
     def post_receipt(self, board_creatio_id):
         """ Создать экземпляр SLReceipt  в Creatio """
         dict_data = {
             'SLTrelloDeskId': board_creatio_id,
         }
-        return self.create_object(RECEIPT_OBJECT_NAME, dict_data)
+        return self.create_object('SLReceipt', dict_data)
     
-    # def create_message_log_sms(
-    #     self,
-    #     mobie_phone,
-    #     text,
-    # ):
-    #     dict_data = {
-    #         'Address': mobie_phone,
-    #         'Text': text,
-    #         'MessageChannelId': 'F7135347-9F65-4573-B409-6ADA0C47ADB6' # SMS
-    #     }
-    #     return self.create_object('MessageLog', data=dict_data)
-
+    def create_message_log_sms(
+        self,
+        mobie_phone,
+        text,
+    ):
+        dict_data = {
+            'Address': mobie_phone,
+            'Text': text,
+            'MessageChannelId': 'F7135347-9F65-4573-B409-6ADA0C47ADB6' # SMS
+        }
+        return self.create_object('MessageLog', data=dict_data)
     
-    # def post_phone_book(self, full_name, lead_id, phone_number):
-    #     dict_data = {
-    #         'UsrFullName': full_name,
-    #         'UsrLeadId': lead_id,
-    #         'UsrNumber': phone_number,
-    #     }     
-    #     return self.create_object(PHONE_BOOK_OBJECT_NAME, dict_data)
-
-    # def delete_receipt(self, receipt_creatio_id):
-    #     status_code: int = self.delete_object(RECEIPT_OBJECT_NAME, receipt_creatio_id)
-    #     return status_code
-
-    # def post_task(self, title, executor_creatio_id, receipt_creatio_id, hours, minutes, card_url):
-    #     """ Создать экземпляр SLTrelloTask """
-    #     dict_data = {
-    #         'SLName': title,
-    #         'SLExecutorId': executor_creatio_id,
-    #         'SLHours': hours,
-    #         'SLMinutes': minutes,
-    #         'SLCardLink': card_url,
-    #         'SLReceiptId': receipt_creatio_id,
-    #     }
-    #     return self.create_object(TASK_OBJECT_NAME, dict_data)
-
-    # def receipt_tasks_count(self, receipt_creatio_id):
-    #     """ Количество тасков в рецепте в Creatio """
-    #     if self.odata_version == ODATA_version.v3:
-    #         url = (
-    #             f"{self.odata_service_link}/{TASK_OBJECT_NAME}Collection" +
-    #             f"?$filter={RECEIPT_OBJECT_NAME}/Id eq guid'{receipt_creatio_id}'"
-    #         )
-    #         response = requests.get(
-    #             url= url,
-    #             headers=self.headers,
-    #             cookies= self.cookies,
-    #         )
-    #         array = json.loads(response.content)['d']['results']
-    #     else:
-    #         url = (
-    #             f"{self.odata_service_link}/{TASK_OBJECT_NAME}" +
-    #             f"?$filter={RECEIPT_OBJECT_NAME}/Id eq {receipt_creatio_id}"
-    #         )
-    #         response = requests.get(
-    #             url= url,
-    #             headers=self.headers,
-    #             cookies= self.cookies,
-    #         )
-    #         array = json.loads(response.content)['value']
-    #     return len(array)
-    
-    def get_object_text_filter(self, object_name: str, field:str, value: str, order_by: str, order_asc: bool = True):
-        """
-            Object collection using filter
-            doesnt support Id columns
-        """
-        if self.odata_version == ODATA_version.v3:
-            if order_asc:
-                asc = 'asc'
-            else:
-                asc = 'desc'
-             
-            url = self.odata_service_link + f"/{object_name}Collection?$filter={field} eq '{value}'&$orderby= {order_by} {asc}"
-            response = requests.get(
-                url=url,
-                headers=self.headers,
-                cookies= self.cookies,
-            )
-            result = json.loads(response.content)['d']['results']
-        else:
-            url = self.odata_service_link + f"/{object_name}?$filter={field} eq '{value}'"
-            response = requests.get(
-                url=url,
-                headers=self.headers,
-                cookies= self.cookies,
-            )
-            result = json.loads(response.content)['value']
-        return result
-    
-    def get_object_by_id(self, object_name: str, object_id: str, order_by:str|None = None, order_direction: OrderDirection = OrderDirection.ascending) -> dict|None:
-        if self.odata_version == ODATA_version.v3:
-            # url = self.odata_service_link + f"/ContactCollection(guid'{contact_id}')?$select=UsrTotalLoansCount, MobilePhone, UsrOpenedLoansCount, Name, Id"
-            url = self.odata_service_link + f"/{object_name}Collection(guid'{object_id}')"
-            response = requests.get(
-                url=url,
-                headers=self.headers,
-                cookies= self.cookies,
-            )
-            result = json.loads(response.content)['d']
-        elif self.odata_version == ODATA_version.v4core or self.odata_version == ODATA_version.v4:
-            url = self.odata_service_link + f"/{object_name}({object_id})"
-            response = requests.get(
-                url=url,
-                headers=self.headers,
-                cookies= self.cookies,
-            )
-            result = json.loads(response.content)
-        else: result = None
-
-        return result
-
-    def get_creatio_contact_id(self, creatio_channel_id, number):
-        if self.odata_version == ODATA_version.v3:
-            url = self.odata_service_link + f"/ContactCommunicationCollection?$filter=CommunicationType/Id eq guid'{creatio_channel_id}' and Number eq '{number}'"
-            response = requests.get(
-                url=url,
-                headers=self.headers,
-                cookies= self.cookies,
-            )
-            result = json.loads(response.content)['d']
-            try:
-                result = result[0]['ContactId']
-            except:
-                result = None
-        elif self.odata_version == ODATA_version.v4core or self.odata_version == ODATA_version.v4:
-            url = self.odata_service_link + f"/ContactCommunication?$filter=CommunicationType/Id eq {creatio_channel_id} and Number eq '{number}'"
-            response = requests.get(
-                url=url,
-                headers=self.headers,
-                cookies= self.cookies,
-            )
-            result = json.loads(response.content)
-            try:
-                result = result[0]['ContactId']
-            except:
-                result = None
-        else: result = None
-
-        return result
-    
-    # Typical implementations
-
-    def get_contact_by_id(self, contact_id):
-        if contact_id == None: return None
-        return self.get_object_id_filter(
-            object_name= 'Contact',
-            object_id= contact_id
-        )
+    def post_phone_book(self, full_name, lead_id, phone_number):
+        dict_data = {
+            'UsrFullName': full_name,
+            'UsrLeadId': lead_id,
+            'UsrNumber': phone_number,
+        }     
+        return self.create_object('SLPhoneBook', dict_data)
---
+++ tests/test_main.py
@@ -0,0 +1,99 @@
+from sl_creatio_connector.constants import ODATA_version
+from sl_creatio_connector.creatio import Creatio
+import pytest
+from os import getenv
+
+
+def test_get_contact_leads_v4():
+    cr = Creatio(
+        creatio_host='http://creatio.simplelogic.ru:5000',
+        login='Vova',
+        password=getenv('SL_CREATIO_PASS'),  # export SL_CREATIO_PASS="my_massword"
+        odata_version=ODATA_version.v4core
+    )
+    parameters = [
+        "filter=Contact eq 'Marady Esther'"
+    ]
+    collection = cr.get_object_collection(
+        object_name='Lead',
+        parameters= parameters,
+    )
+    assert len(collection) == 0
+
+def test_get_contact_leads_v3():
+    cr = Creatio(
+        creatio_host='http://crm.monusluy.com',
+        login='Supervisor',
+        password=getenv('CAMBO_CREATIO_PASS'),  # export CAMBO_CREATIO_PASS="my_massword"
+        odata_version=ODATA_version.v3
+    )
+    parameters = [
+        "filter=Contact eq 'Marady Esther'"
+    ]
+    collection = cr.get_object_collection(
+        object_name='Lead',
+        parameters= parameters,
+    )
+    assert len(collection) == 2
+
+def test_get_contact_by_id_v4():
+    cr = Creatio(
+        creatio_host='http://creatio.simplelogic.ru:5000',
+        login='Vova',
+        password=getenv('SL_CREATIO_PASS'),  # export SL_CREATIO_PASS="my_massword"
+        odata_version=ODATA_version.v4core
+    )
+    cr.get_contact_by_id('b10e4cef-66d6-46ce-9479-83a125c750ae')
+
+def test_get_contact_by_id_v3():
+    cr = Creatio(
+        creatio_host='http://crm.monusluy.com',
+        login='Supervisor',
+        password=getenv('CAMBO_CREATIO_PASS'),  # export CAMBO_CREATIO_PASS="my_massword"
+        odata_version=ODATA_version.v3
+    )
+    cr.get_contact_by_id('b2a8c568-002f-4fd1-a15a-ffda98f5f63b')
+
+def test_create_and_delete_contact_by_id_v3():
+    cr = Creatio(
+        creatio_host='http://creatio.simplelogic.ru:5000',
+        login='Vova',
+        password=getenv('SL_CREATIO_PASS'),  # export SL_CREATIO_PASS="my_massword"
+        odata_version=ODATA_version.v4core
+    )
+    data = {
+        'Name': "Test name"
+    }
+    created_contact = cr.create_object(
+        'Contact',
+        data= data,
+    )
+    created_id = created_contact['Id']
+    status_code = cr.delete_object('Contact', created_id).status_code
+    assert status_code == 204
+
+def test_create_and_delete_contact_by_id_v4():
+    cr = Creatio(
+        creatio_host='http://crm.monusluy.com',
+        login='Supervisor',
+        password=getenv('CAMBO_CREATIO_PASS'),  # export CAMBO_CREATIO_PASS="my_massword"
+        odata_version=ODATA_version.v3
+    )
+    data = {
+        'Name': "Test name"
+    }
+    created_contact = cr.create_object(
+        'Contact',
+        data= data,
+    )
+    created_id = created_contact['Id']
+    # status_code = cr.delete_object('Contact', created_id).status_code
+    # assert status_code == 204
+
+if __name__ == '__main__':
+    test_get_contact_leads_v4()
+    test_get_contact_leads_v3()
+    test_get_contact_by_id_v3()
+    test_get_contact_by_id_v4()
+    test_create_and_delete_contact_by_id_v3()
+    test_create_and_delete_contact_by_id_v4()
